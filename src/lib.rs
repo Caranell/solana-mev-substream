e062e11b16b4a780db667b9afb59499215e1c993
Caranell <lyachek.anton@gmail.com>
2025-05-05 00:45:36 +0300

    Scaffolding

--- src/lib.rs
+++ src/lib.rs
@@ -1,96 +1,110 @@
-mod abi;
+#![allow(dead_code)]
+#![allow(unused_variables)]
+#![allow(non_snake_case)]
+
 mod pb;
-use hex_literal::hex;
-use pb::eth::erc721::v1 as erc721;
-use substreams::{key, prelude::*};
-use substreams::{log, store::StoreAddInt64, Hex};
-use substreams_database_change::pb::database::DatabaseChanges;
-use substreams_database_change::tables::Tables;
-use substreams_ethereum::pb::sf::ethereum::r#type::v2 as eth;
+// mod utils;
 
-// Bored Ape Club Contract
-const TRACKED_CONTRACT: [u8; 20] = hex!("bc4ca0eda7647a8ab7c2061c2e118a18a936f13d");
+use std::collections::HashMap;
 
-substreams_ethereum::init!();
+use pb::caranell::solana::mev::bundles::v1::{MevBundle, Output, TradeData};
 
-/// Extracts transfers events from the contract
 #[substreams::handlers::map]
-fn map_transfers(blk: eth::Block) -> Result<Option<erc721::Transfers>, substreams::errors::Error> {
-    let transfers: Vec<_> = blk
-        .events::<abi::erc721::events::Transfer>(&[&TRACKED_CONTRACT])
-        .map(|(transfer, log)| {
-            substreams::log::info!("NFT Transfer seen");
+fn map_trades(trades: Vec<TradeData>) -> Result<Output, substreams::errors::Error> {
+    let mev_bundles = find_mev_bundles(trades);
 
-            erc721::Transfer {
-                trx_hash: Hex::encode(&log.receipt.transaction.hash),
-                from: Hex::encode(&transfer.from),
-                to: Hex::encode(&transfer.to),
-                token_id: transfer.token_id.to_u64(),
-                ordinal: log.block_index() as u64,
+    return Ok(Output {
+        bundles: mev_bundles,
+    });
+}
+
+fn find_mev_bundles(trade_data: Vec<TradeData>) -> () {
+    let mut mev_bundles = Vec::new();
+
+    let mut arbitrage_bundles = Vec::new();
+
+    let mut arbitrage_sequence: Vec<TradeData> = Vec::new();
+
+    let mut arbitrage_sequence_broken = true;
+    for (idx, trade) in trade_data.iter().enumerate() {
+        if let Some(prev_trade) = arbitrage_sequence.last() {
+            if is_same_transaction(trade, prev_trade) {
+                arbitrage_sequence.push(trade.clone());
+                arbitrage_sequence_broken = false;
             }
-        })
-        .collect();
-    if transfers.len() == 0 {
-        return Ok(None);
-    }
-
-    Ok(Some(erc721::Transfers { transfers }))
-}
-
-const NULL_ADDRESS: &str = "0000000000000000000000000000000000000000";
-
-/// Store the total balance of NFT tokens for the specific TRACKED_CONTRACT by holder
-#[substreams::handlers::store]
-fn store_transfers(transfers: erc721::Transfers, s: StoreAddInt64) {
-    log::info!("NFT holders state builder");
-    for transfer in transfers.transfers {
-        if transfer.from != NULL_ADDRESS {
-            log::info!("Found a transfer out {}", Hex(&transfer.trx_hash));
-            s.add(transfer.ordinal, generate_key(&transfer.from), -1);
+        } else {
+            // it's potential first trade in a sequence
+            arbitrage_sequence.push(trade.clone());
+            arbitrage_sequence_broken = false;
         }
 
-        if transfer.to != NULL_ADDRESS {
-            log::info!("Found a transfer in {}", Hex(&transfer.trx_hash));
-            s.add(transfer.ordinal, generate_key(&transfer.to), 1);
+        if arbitrage_sequence_broken {
+            // sequence broken, check if the completed sequence was an arbitrage (>= 2 trades)
+            if arbitrage_sequence.len() > 1 {
+                arbitrage_bundles.push(arbitrage_sequence);
+                break;
+            }
+
+            arbitrage_sequence = vec![trade.clone()];
+        }
+
+        // Sandwich detection
+        if idx >= 2 {
+            let trade_a = &trade_data[idx - 2];
+            let trade_b = &trade_data[idx - 1];
+            let trade_c = &trade_data[idx];
+
+            // Core sandwich condition: A and C have same signer, different from B's signer
+            if trade_a.signer == trade_c.signer && trade_a.signer != trade_b.signer {
+                arbitrage_bundles.push(vec![trade_a.clone(), trade_b.clone(), trade_c.clone()]);
+            }
         }
     }
+
+    return ();
 }
 
-#[substreams::handlers::map]
-fn db_out(
-    clock: substreams::pb::substreams::Clock,
-    transfers: erc721::Transfers,
-    owner_deltas: Deltas<DeltaInt64>,
-) -> Result<DatabaseChanges, substreams::errors::Error> {
-    let mut tables = Tables::new();
-    for transfer in transfers.transfers {
-        tables
-            .create_row(
-                "transfer",
-                format!("{}-{}", &transfer.trx_hash, transfer.ordinal),
-            )
-            .set("trx_hash", transfer.trx_hash)
-            .set("from", transfer.from)
-            .set("to", transfer.to)
-            .set("token_id", transfer.token_id)
-            .set("ordinal", transfer.ordinal);
+fn is_same_transaction(trade: &TradeData, prev_trade: &TradeData) -> bool {
+    if trade.tx_index == prev_trade.tx_index && trade.signer == prev_trade.signer {
+        return true;
     }
 
-    for delta in owner_deltas.into_iter() {
-        let holder = key::segment_at(&delta.key, 1);
-        let contract = key::segment_at(&delta.key, 2);
-
-        tables
-            .create_row("owner_count", format!("{}-{}", contract, holder))
-            .set("contract", contract)
-            .set("holder", holder)
-            .set("balance", delta.new_value)
-            .set("block_number", clock.number);
-    }
-
-    Ok(tables.to_database_changes())
+    return false;
 }
 
-fn generate_key(holder: &String) -> String {
-    return format!("total:{}:{}", holder, Hex(TRACKED_CONTRACT));
+
+fn process_mev_trades(trade_data: Vec<TradeData>) -> Vec<MevBundle> {
+    let mut mev_trades = HashMap::new();
+    for trade in trade_data {
+        let signer = trade.signer.clone();
+        let signer_trades = mev_trades.entry(signer).or_insert(vec![]);
+
+        signer_trades.push(trade);
+    }
+
+    // filter out signers with only 1 trade
+    mev_trades.retain(|_, trades| trades.len() > 1);
+
+    let mut arbitrage_trades = Vec::new();
+    for (signer, trades) in mev_trades {
+        let trades = trades.clone();
+
+        // group trades by tx_index
+        let mut trades_by_tx_index = HashMap::new();
+        for trade in trades {
+            let tx_index = trade.tx_index;
+            let trade_data = trades_by_tx_index.entry(tx_index).or_insert(vec![]);
+            trade_data.push(trade);
+        }
+
+        // for each tx_index, if there're multiple trades, it's arbitrage
+        for (tx_index, trades) in trades_by_tx_index {
+            if trades.len() > 1 {
+                let result = get_arbitrage_summary(trades);
+                arbitrage_trades.push(result);
+            }
+        }
+    }
+
+    return arbitrage_trades;
 }
--- src/pb/mod.rs
+++ src/pb/mod.rs
@@ -1,10 +1,41 @@
 // @generated
-pub mod eth {
-    pub mod erc721 {
-        // @@protoc_insertion_point(attribute:eth.erc721.v1)
+pub mod caranell {
+    pub mod solana {
+        pub mod mev {
+            pub mod bundles {
+                // @@protoc_insertion_point(attribute:caranell.solana.mev.bundles.v1)
+                pub mod v1 {
+                    include!("caranell.solana.mev.bundles.v1.rs");
+                    // @@protoc_insertion_point(caranell.solana.mev.bundles.v1)
+                }
+            }
+        }
+    }
+}
+pub mod sf {
+    pub mod solana {
+        pub mod r#type {
+            // @@protoc_insertion_point(attribute:sf.solana.type.v1)
+            pub mod v1 {
+                include!("sf.solana.type.v1.rs");
+                // @@protoc_insertion_point(sf.solana.type.v1)
+            }
+        }
+    }
+}
+pub mod sol {
+    pub mod instructions {
+        // @@protoc_insertion_point(attribute:sol.instructions.v1)
         pub mod v1 {
-            include!("eth.erc721.v1.rs");
-            // @@protoc_insertion_point(eth.erc721.v1)
+            include!("sol.instructions.v1.rs");
+            // @@protoc_insertion_point(sol.instructions.v1)
+        }
+    }
+    pub mod transactions {
+        // @@protoc_insertion_point(attribute:sol.transactions.v1)
+        pub mod v1 {
+            include!("sol.transactions.v1.rs");
+            // @@protoc_insertion_point(sol.transactions.v1)
         }
     }
 }
