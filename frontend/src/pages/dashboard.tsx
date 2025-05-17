0eda28599122a8ed6f43d14fb2fc45a8c8f08991
Caranell <lyachek.anton@gmail.com>
2025-05-17 10:36:06 +0300

    Set default time period 1D

--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -17,7 +17,7 @@ const MEV_TYPES_LABELS = {
 };
 
 export function Dashboard() {
-  const [timeFilter, setTimeFilter] = useState<string>(TIME_MAPPING["7D"]);
+  const [timeFilter, setTimeFilter] = useState<string>(TIME_MAPPING["1D"]);
   const [mevType, setMevType] = useState(MEV_TYPES.ARBITRAGE);
 
   const { data: transactions } = useQuery({
